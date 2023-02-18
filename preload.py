03a8fc0bf9131c951ea82beda67aefb81bd17fb5
brkirch <brkirch@users.noreply.github.com>
2023-02-18 01:04:07 -0500

    Support half precision, older web UI versions

--- preload.py
+++ preload.py
@@ -1,2 +1,3 @@
 def preload(parser):
     parser.add_argument("--controlnet-dir", type=str, help="Path to directory with ControlNet models", default=None)
+    parser.add_argument("--no-half-controlnet", action='store_true', help="do not switch the ControlNet models to 16-bit floats (only needed without --no-half)", default=None)
