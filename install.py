d0a43a95fb47231bd234015ae53172c9c486ccf1
Mikubill <31246794+Mikubill@users.noreply.github.com>
2023-03-04 12:02:58 +0000

    fix: use opemmim to configure mm packages

--- install.py
+++ install.py
@@ -3,8 +3,21 @@ import os
 
 req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
 
+mim_packages = ["mmcv-full", "mmsegmentation", "mmdet"]
+
+def run_mim(args, desc=None):
+    if launch.skip_install:
+        return
+
+    index_url_line = f' --index-url {launch.index_url}' if launch.index_url != '' else ''
+    return launch.run(f'"{launch.python}" -m mim {args} --prefer-binary{index_url_line}', desc=f"Installing {desc}", errdesc=f"Couldn't install {desc}")
+
 with open(req_file) as file:
     for lib in file:
         lib = lib.strip()
         if not launch.is_installed(lib):
             launch.run_pip(f"install {lib}", f"sd-webui-controlnet requirement: {lib}")
+
+run_mim(f"install {' '.join(mim_packages)}", f"sd-webui-controlnet requirement: {', '.join(mim_packages)}")
+            
+
