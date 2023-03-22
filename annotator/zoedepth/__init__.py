aceb58ff2ec94ff371f0e9d9e2fb400bd9e67a2a
thibaudart <103603084+thibaudart@users.noreply.github.com>
2023-03-22 21:33:20 +0100

    Update __init__.py

--- annotator/zoedepth/__init__.py
+++ annotator/zoedepth/__init__.py
@@ -13,10 +13,27 @@ def unload_zoedepth():
 def apply_zoedepth(img, res=512):
     global model_zoedepth
 
+    if model_zoedepth is None:
+        DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+        if not os.path.exists("DPT_BEiT_L_384.txt"):
+            torch.hub.help("intel-isl/MiDaS", "DPT_BEiT_L_384", force_reload=True)  # Triggers fresh download of MiDaS repo
+            open("DPT_BEiT_L_384.txt","w")
+        model_zoedepth = torch.hub.load('isl-org/ZoeDepth', "ZoeD_NK", pretrained=True, force_reload=True).to(DEVICE).eval()
+
+    img_zoedepth = model_zoedepth.infer_pil(img)
+    img_zoedepth_colored_depth = Image.fromarray(colorize(img_zoedepth)).convert('L')
+
+    return img_zoedepth_colored_depth
+
+def apply_colored_zoedepth(img, res=512):
+    global model_zoedepth
+
 
     if model_zoedepth is None:
         DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-        torch.hub.help("intel-isl/MiDaS", "DPT_BEiT_L_384", force_reload=True)  # Triggers fresh download of MiDaS repo
+        if not os.path.exists("DPT_BEiT_L_384.txt"):
+            torch.hub.help("intel-isl/MiDaS", "DPT_BEiT_L_384", force_reload=True)  # Triggers fresh download of MiDaS repo
+            open("DPT_BEiT_L_384.txt","w")
         model_zoedepth = torch.hub.load('isl-org/ZoeDepth', "ZoeD_NK", pretrained=True, force_reload=True).to(DEVICE).eval()
 
     img_zoedepth = model_zoedepth.infer_pil(img)
