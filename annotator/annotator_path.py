d62badd9d1568f84d70ad7d8284965801a97df0f
lvmin <lyuminzhang@outlook.com>
2023-04-18 09:06:55 -0700

    add sanity check to log so that reading github issue is easier

--- annotator/annotator_path.py
+++ annotator/annotator_path.py
@@ -9,11 +9,11 @@ if not models_path:
 
 clip_vision_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'clip_vision')
 # clip vision is always inside controlnet "extensions\sd-webui-controlnet"
-# and any problem can be solved by removing and reinstall
+# and any problem can be solved by removing controlnet and reinstall
 
 models_path = os.path.realpath(models_path)
 os.makedirs(models_path, exist_ok=True)
 print(f'ControlNet preprocessor location: {models_path}')
 # Make sure that the default location is inside controlnet "extensions\sd-webui-controlnet"
-# so that any problem can be solved by removing and reinstall
+# so that any problem can be solved by removing controlnet and reinstall
 # if users do not change configs on their own (otherwise users will know what is wrong)
