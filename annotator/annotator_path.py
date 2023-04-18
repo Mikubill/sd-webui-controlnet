804d32ee3f86fd591a6411f0fa0a646b08953293
ljleb <lebel.louisjacob@gmail.com>
2023-04-18 06:16:59 -0400

    add options

--- annotator/annotator_path.py
+++ annotator/annotator_path.py
@@ -1,4 +1,11 @@
 import os
+from modules import shared
 
-models_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'downloads')
+models_path = shared.opts.data.get('control_net_modules_path', None)
+if not models_path:
+    models_path = getattr(shared.cmd_opts, 'controlnet_annotator_models_path', None)
+if not models_path:
+    models_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'downloads')
+
+models_path = os.path.realpath(models_path)
 os.makedirs(models_path, exist_ok=True)
