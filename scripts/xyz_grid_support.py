475e47aae57b0e1e87c827e68b6a91c2cedf6404
ljleb <lebel.louisjacob@gmail.com>
2023-04-22 00:08:01 -0400

    forward xyz grid support

---
+++ scripts/xyz_grid_support.py
@@ -0,0 +1,4 @@
+from lib_controlnet.xyz_grid_support import *
+
+# this file only exists for backwards compatibility.
+# to update or add features to the xyz grid support, please look into the source of the module imported above
