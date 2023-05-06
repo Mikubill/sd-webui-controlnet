c39d9d6c5269c26add917e5476099325dd241dac
lvmin <lyuminzhang@outlook.com>
2023-05-06 10:44:14 -0700

    143

--- scripts/controlnet_version.py
+++ scripts/controlnet_version.py
@@ -1,4 +1,4 @@
-version_flag = 'v1.1.142'
+version_flag = 'v1.1.143'
 print(f'ControlNet {version_flag}')
 # A smart trick to know if user has updated as well as if user has restarted terminal.
 # Note that in "controlnet.py" we do NOT use "importlib.reload" to reload this "controlnet_version.py"
