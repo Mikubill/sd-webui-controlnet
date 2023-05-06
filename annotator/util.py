99008f6b86547432d9b1d81e66c112ebe7d1f4f4
lvmin <lyuminzhang@outlook.com>
2023-05-06 10:32:50 -0700

    rename

--- annotator/util.py
+++ annotator/util.py
@@ -21,17 +21,18 @@ def HWC3(x):
         return y
 
 
-def resize_image(input_image, resolution):
-    H, W, C = input_image.shape
-    H = float(H)
-    W = float(W)
-    k = float(resolution) / min(H, W)
-    H *= k
-    W *= k
-    H = int(np.round(H / 64.0)) * 64
-    W = int(np.round(W / 64.0)) * 64
-    img = cv2.resize(input_image, (W, H), interpolation=cv2.INTER_LANCZOS4 if k > 1 else cv2.INTER_AREA)
-    return img
+# Removed - Do not use this!
+# def resize_image(input_image, resolution):
+#     H, W, C = input_image.shape
+#     H = float(H)
+#     W = float(W)
+#     k = float(resolution) / min(H, W)
+#     H *= k
+#     W *= k
+#     H = int(np.round(H / 64.0)) * 64
+#     W = int(np.round(W / 64.0)) * 64
+#     img = cv2.resize(input_image, (W, H), interpolation=cv2.INTER_LANCZOS4 if k > 1 else cv2.INTER_AREA)
+#     return img
 
 
 def make_noise_disk(H, W, C, F):
