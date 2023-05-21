84588c58fddf0dab5e02edff534426862c816886
huchenlei <chenlei.hu@mail.utoronto.ca>
2023-05-21 14:26:10 -0400

    :bug: Add back cmd arguments that accidentally got removed

--- preload.py
+++ preload.py
@@ -1,4 +1,7 @@
 def preload(parser):
+    parser.add_argument("--controlnet-dir", type=str, help="Path to directory with ControlNet models", default=None)
+    parser.add_argument("--controlnet-annotator-models-path", type=str, help="Path to directory with annotator model directories", default=None)
+    parser.add_argument("--no-half-controlnet", action='store_true', help="do not switch the ControlNet models to 16-bit floats (only needed without --no-half)", default=None)
     # Setting default max_size=16 as each cache entry contains image as both key 
     # and value (Very costly).
     parser.add_argument(
