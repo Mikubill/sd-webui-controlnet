b41d536abce00f90a0b55ba3caf868442467b48a
lllyasviel <lyuminzhang@outlook.com>
2023-05-21 19:02:19 -0700

    more friendly assert (#1384)

--- scripts/hook.py
+++ scripts/hook.py
@@ -50,8 +50,8 @@ def mark_prompt_context(x, positive):
 def unmark_prompt_context(x):
     if not prompt_context_is_marked(x):
         print('ControlNet Error: Failed to detect whether an instance is cond or uncond!')
-        print('ControlNet Error: This is mainly because other extension(s) blocked A1111\'s \"process.sample()\" and deleted ControlNet\' sample function.')
-        print('ControlNet Error: ControlNet will shift to backup backend but the results will be worse than expectation.')
+        print('ControlNet Error: This is mainly because other extension(s) blocked A1111\'s \"process.sample()\" and deleted ControlNet\'s sample function.')
+        print('ControlNet Error: ControlNet will shift to a backup backend but the results will be worse than expectation.')
         print('Solution (For extension developers): Take a look at ControlNet\' hook.py '
               'UnetHook.hook.process_sample and manually call mark_prompt_context to mark cond/uncond prompts.')
         mark_batch = torch.ones(size=(x.shape[0], 1, 1, 1), dtype=x.dtype, device=x.device)
