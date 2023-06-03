611e734b9bf1c67a3a33321f990cdb7755cf657d
huchenlei <chenlei.hu@mail.utoronto.ca>
2023-06-03 17:18:35 -0400

    nits

--- scripts/logging.py
+++ scripts/logging.py
@@ -11,13 +11,6 @@ handler.setFormatter(
     logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 )
 logger.addHandler(handler)
-
-
-if hasattr(shared.cmd_opts, "controlnet_loglevel"):
-    loglevel_string = shared.cmd_opts.controlnet_loglevel
-else:
-    print("Warning: `controlnet_loglevel` not available from A1111")
-    loglevel_string = "DEBUG"
-
+loglevel_string = getattr(shared.cmd_opts, "controlnet_loglevel", "INFO")
 loglevel = getattr(logging, loglevel_string.upper(), None)
 logger.setLevel(loglevel)
