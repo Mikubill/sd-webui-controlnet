16805be062f44feee4513a39349a1ed83595279e
Chenlei Hu <chenlei.hu@mail.utoronto.ca>
2023-06-22 21:38:12 -0400

    [Reland] 🔨 Refactor bound check for threshold and resolution (#1675)
    * :hammer: Refactor bound check for threshold and resolution (#1668)
    
    * :hammer: Refactor bound check for threshold and resolution
    
    * nits
    
    * :bug: Fix bound check. Add more unittest.
    
    * Fix param value used in logs

--- tests/cn_script/cn_script_test.py
+++ tests/cn_script/cn_script_test.py
@@ -6,7 +6,8 @@ import importlib
 utils = importlib.import_module('extensions.sd-webui-controlnet.tests.utils', 'utils')
 utils.setup_test_env()
 
-from scripts.controlnet import prepare_mask
+from scripts import external_code, processor
+from scripts.controlnet import prepare_mask, Script
 from modules import processing
 
 
@@ -38,6 +39,27 @@ class TestPrepareMask(unittest.TestCase):
 
         # Check that mask is not blurred when 'mask_blur' is 0
         self.assertEqual(processed_mask.getpixel((0, 0)), 0)  # black should remain black
+
+
+class TestScript(unittest.TestCase):
+    def test_bound_check_params(self):
+        def param_required(module: str, param: str) -> bool:
+            configs = processor.preprocessor_sliders_config[module]
+            config_index = ('processor_res', 'threshold_a', 'threshold_b').index(param)
+            return config_index < len(configs) and configs[config_index] is not None
+
+        for module in processor.preprocessor_sliders_config.keys():
+            for param in ('processor_res', 'threshold_a', 'threshold_b'):
+                with self.subTest(param=param, module=module):
+                    unit = external_code.ControlNetUnit(
+                        module=module,
+                        **{param: -100},
+                    )
+                    Script.bound_check_params(unit)
+                    if param_required(module, param):
+                        self.assertGreaterEqual(getattr(unit, param), 0)
+                    else:
+                        self.assertEqual(getattr(unit, param), -100)
 
 
 if __name__ == "__main__":
