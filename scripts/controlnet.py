caddb91d49008f7876bf58beb4ca6f66ffa60e85
ljleb <set>
2023-07-22 22:40:50 -0400

    dont assume images is tensor

--- scripts/controlnet.py
+++ scripts/controlnet.py
@@ -934,7 +934,7 @@ class Script(scripts.Script, metaclass=(
     def postprocess_batch(self, p, *args, **kwargs):
         images = kwargs.get('images', [])
         for post_processor in self.post_processors:
-            for i in range(images.shape[0]):
+            for i in range(len(images)):
                 images[i] = post_processor(images[i])
         return
 
