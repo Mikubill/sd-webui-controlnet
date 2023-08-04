0ee510c659acbe3c308e33cefa89381565eb6600
Chenlei Hu <chenlei.hu@mail.utoronto.ca>
2023-08-04 15:02:58 -0400

    Port DW Pose preprocessor (#1856)
    * :heavy_plus_sign: Add dependencies
    
    * :construction: wip
    
    * :construction: wip
    
    * :construction: download models
    
    * :construction: Minor fixes
    
    * :wrench: update gitignore
    
    * :bug: Fix normalization issue
    
    * :construction: load DW model only when DW preprocessor is selected
    
    * :white_check_mark: Change test config
    
    * :art: nits
    
    * :bug: Fix A1111 safe torch issue
    
    :memo: v1.1.235 (#1859)
    
    Revert "Port DW Pose preprocessor (#1856)" (#1860)
    
    This reverts commit 0d3310f5997ed63c48971040012701817fc7249c.
    
    Reland "Port DW Pose preprocessor" (#1861)
    
    * Revert "Revert "Port DW Pose preprocessor (#1856)" (#1860)"
    
    This reverts commit 17e100e1fefb56bb53b91c9c498b81c578cec51a.
    
    * :bug: Fix install.py
    
    :memo: v1.1.236 (#1862)
    
    :bug: Delay import of mmpose (#1866)
    
    :memo: v1.1.237 (#1868)
    
    :bug: Fix all keypoints invalid issue (#1871)
    
    :bug: lazy import
    
    :construction: update test expectation
    
    :construction: Switch to onnx
    
    :construction: solve onnx package issue
    
    :wrench: Check cuda in more efficient way
    
    :art: Format code
    
    :wrench: Make onnx runtime optional

--- annotator/openpose/body.py
+++ annotator/openpose/body.py
@@ -11,24 +11,7 @@ from typing import NamedTuple, List, Union
 
 from . import util
 from .model import bodypose_model
-
-class Keypoint(NamedTuple):
-    x: float
-    y: float
-    score: float = 1.0
-    id: int = -1
-
-
-class BodyResult(NamedTuple):
-    # Note: Using `Union` instead of `|` operator as the ladder is a Python
-    # 3.10 feature.
-    # Annotator code should be Python 3.8 Compatible, as controlnet repo uses
-    # Python 3.8 environment.
-    # https://github.com/lllyasviel/ControlNet/blob/d3284fcd0972c510635a4f5abe2eeb71dc0de524/environment.yaml#L6
-    keypoints: List[Union[Keypoint, None]]
-    total_score: float = 0.0
-    total_parts: int = 0
-
+from .types import Keypoint, BodyResult
 
 class Body(object):
     def __init__(self, model_path):
