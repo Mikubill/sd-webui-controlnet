2b17833875002910e3845acf88ed3def06475ef7
huchenlei <chenlei.hu@mail.utoronto.ca>
2023-08-04 15:45:52 -0400

    :bug: Fix install.py

--- install.py
+++ install.py
@@ -1,7 +1,6 @@
 import launch
 import os
 import pkg_resources
-import subprocess
 
 req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
 
@@ -20,18 +19,31 @@ with open(req_file) as file:
             print(e)
             print(f'Warning: Failed to install {package}, some preprocessors may not work.')
 
-
 # DW Pose dependencies.
+def install_mim():
+    import importlib
+    module_spec = importlib.util.find_spec('mim')
+    if module_spec is None:
+        launch.run_pip("install openmim", "sd-webui-controlnet requirement: openmim")
+
 mim_packages = [
     "mmengine",
     "mmcv>=2.0.1",
     "mmdet>=3.1.0",
     "mmpose>=1.1.0",
 ]
-for package in mim_packages:
-    try:
-        package_name = package.split('>=')[0]
-        if not launch.is_installed(package_name):
-            subprocess.call(["mim", "install", package])
-    except Exception as e:
-        print(f'Warning: Failed to install {package}. {e}')
+
+def install_mim_packages(packages):
+    import mim
+    for package in mim_packages:
+        packages = {p for (p, *_) in mim.list_package()}
+        try:
+            package_name = package.split('>=')[0]
+            if package_name not in packages:
+                mim.install([package])
+        except Exception as e:
+            print(f'Warning: Failed to install {package}. {e}')
+            raise e
+
+install_mim()
+install_mim_packages(mim_packages)
