9eeea258cfca99693df847e15d1a7974c08405e4
huchenlei <chenlei.hu@mail.utoronto.ca>
2023-08-04 21:24:52 -0400

    :bug: Fix all keypoints invalid issue

--- annotator/openpose/util.py
+++ annotator/openpose/util.py
@@ -68,7 +68,15 @@ def transfer(model, model_weights):
 
 
 def is_normalized(keypoints: List[Optional[Keypoint]]) -> bool:
-    return all(0 <= abs(k.x) <= 1 and 0 <= abs(k.y) <= 1 for k in keypoints if k is not None)
+    point_normalized = [
+        0 <= abs(k.x) <= 1 and 0 <= abs(k.y) <= 1 
+        for k in keypoints 
+        if k is not None
+    ]
+    if not point_normalized:
+        return False
+    return all(point_normalized)
+
     
 def draw_bodypose(canvas: np.ndarray, keypoints: List[Keypoint]) -> np.ndarray:
     """
