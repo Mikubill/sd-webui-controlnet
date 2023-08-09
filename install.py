e391397774a40e7f5e346fc658492f15314c84fb
huchenlei <chenlei.hu@mail.utoronto.ca>
2023-08-09 13:28:44 -0400

    :bug: Add dependency on opencv 4.8.0
    adding -U to pip

--- install.py
+++ install.py
@@ -1,8 +1,21 @@
 import launch
 import os
 import pkg_resources
+from typing import Tuple, Optional
 
 req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
+
+
+def comparable_version(version: str) -> Tuple:
+    return tuple(version.split('.'))
+    
+
+def get_installed_version(package: str) -> Optional[str]:
+    try:
+        return pkg_resources.get_distribution(package).version
+    except Exception:
+        return None
+
 
 with open(req_file) as file:
     for package in file:
@@ -10,9 +23,14 @@ with open(req_file) as file:
             package = package.strip()
             if '==' in package:
                 package_name, package_version = package.split('==')
-                installed_version = pkg_resources.get_distribution(package_name).version
+                installed_version = get_installed_version(package_name)
                 if installed_version != package_version:
-                    launch.run_pip(f"install {package}", f"sd-webui-controlnet requirement: changing {package_name} version from {installed_version} to {package_version}")
+                    launch.run_pip(f"install -U {package}", f"sd-webui-controlnet requirement: changing {package_name} version from {installed_version} to {package_version}")
+            elif '>=' in package:
+                package_name, package_version = package.split('>=')
+                installed_version = get_installed_version(package_name)
+                if not installed_version or comparable_version(installed_version) < comparable_version(package_version):
+                    launch.run_pip(f"install -U {package}", f"sd-webui-controlnet requirement: changing {package_name} version from {installed_version} to {package_version}")
             elif not launch.is_installed(package):
                 launch.run_pip(f"install {package}", f"sd-webui-controlnet requirement: {package}")
         except Exception as e:
