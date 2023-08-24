7ae6187b2013458839f070c5e80892bab6fb443d
w-e-w <40751091+w-e-w@users.noreply.github.com>
2023-08-25 00:07:22 +0900

    make zoom work with single CN module

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -6,6 +6,8 @@
                 hasApplied = true;
                 window.applyZoomAndPanIntegration("#txt2img_controlnet",Array.from({ length: 20 }, (_, i) => `#txt2img_controlnet_ControlNet-${i}_input_image`));
                 window.applyZoomAndPanIntegration("#img2img_controlnet",Array.from({ length: 20 }, (_, i) => `#img2img_controlnet_ControlNet-${i}_input_image`));
+                window.applyZoomAndPan("#txt2img_controlnet_ControlNet_input_image");
+                window.applyZoomAndPan("#img2img_controlnet_ControlNet_input_image");
                 //console.log("window.applyZoomAndPanIntegration applied.");
             } else {
                 //console.log("window.applyZoomAndPanIntegration is not available.");
