84601a03b5fc20085d6d71b99b3898d01d87ef61
lllyasviel <lyuminzhang@outlook.com>
2023-08-24 14:36:32 -0700

    Update canvas.js

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -2,14 +2,12 @@
     var hasApplied = false;
     onUiUpdate(function () {
         if (!hasApplied) {
-            if (typeof window.applyZoomAndPanIntegration === "function") {  // webui 1.6.0 
+            if (typeof window.applyZoomAndPanIntegration === "function") {
                 hasApplied = true;
                 window.applyZoomAndPanIntegration("#txt2img_controlnet",Array.from({ length: 20 }, (_, i) => `#txt2img_controlnet_ControlNet-${i}_input_image`));
                 window.applyZoomAndPanIntegration("#img2img_controlnet",Array.from({ length: 20 }, (_, i) => `#img2img_controlnet_ControlNet-${i}_input_image`));
-                if (typeof window.applyZoomAndPan === "function") {  // check is nested because window.applyZoomAndPan was added in webui 1.4.0
-                    window.applyZoomAndPan("#txt2img_controlnet_ControlNet_input_image");
-                    window.applyZoomAndPan("#img2img_controlnet_ControlNet_input_image");
-                }
+                window.applyZoomAndPanIntegration("#txt2img_controlnet", ["#txt2img_controlnet_ControlNet_input_image"]);
+                window.applyZoomAndPanIntegration("#img2img_controlnet", ["#img2img_controlnet_ControlNet_input_image"]);
                 //console.log("window.applyZoomAndPanIntegration applied.");
             } else {
                 //console.log("window.applyZoomAndPanIntegration is not available.");
