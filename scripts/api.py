96745715ff25d2599dbc2b7d4f2f91760682e611
huchenlei <chenlei.hu@mail.utoronto.ca>
2023-09-03 15:52:57 -0400

    :art: re-format api.py

--- scripts/api.py
+++ scripts/api.py
@@ -23,9 +23,11 @@ def encode_to_base64(image):
     else:
         return ""
 
+
 def encode_np_to_base64(image):
     pil = Image.fromarray(image)
     return api.encode_pil_to_base64(pil)
+
 
 def controlnet_api(_: gr.Blocks, app: FastAPI):
     @app.get("/controlnet/version")
@@ -42,12 +44,12 @@ def controlnet_api(_: gr.Blocks, app: FastAPI):
     async def module_list(alias_names: bool = False):
         _module_list = external_code.get_modules(alias_names)
         logger.debug(_module_list)
-        
+
         return {
             "module_list": _module_list,
-            "module_detail": external_code.get_modules_detail(alias_names)
+            "module_detail": external_code.get_modules_detail(alias_names),
         }
-    
+
     @app.get("/controlnet/control_types")
     async def control_types():
         def format_control_type(
@@ -62,40 +64,48 @@ def controlnet_api(_: gr.Blocks, app: FastAPI):
                 "default_option": default_option,
                 "default_model": default_model,
             }
-        
+
         return {
-            'control_types': {
-                control_type: format_control_type(*global_state.select_control_type(control_type))
+            "control_types": {
+                control_type: format_control_type(
+                    *global_state.select_control_type(control_type)
+                )
                 for control_type in preprocessor_filters.keys()
             }
         }
 
-
     @app.get("/controlnet/settings")
     async def settings():
         max_models_num = external_code.get_max_models_num()
-        return {"control_net_max_models_num":max_models_num}
+        return {"control_net_max_models_num": max_models_num}
 
-    cached_cn_preprocessors = global_state.cache_preprocessors(global_state.cn_preprocessor_modules)
+    cached_cn_preprocessors = global_state.cache_preprocessors(
+        global_state.cn_preprocessor_modules
+    )
+
     @app.post("/controlnet/detect")
     async def detect(
-        controlnet_module: str = Body("none", title='Controlnet Module'),
-        controlnet_input_images: List[str] = Body([], title='Controlnet Input Images'),
-        controlnet_processor_res: int = Body(512, title='Controlnet Processor Resolution'),
-        controlnet_threshold_a: float = Body(64, title='Controlnet Threshold a'),
-        controlnet_threshold_b: float = Body(64, title='Controlnet Threshold b')
+        controlnet_module: str = Body("none", title="Controlnet Module"),
+        controlnet_input_images: List[str] = Body([], title="Controlnet Input Images"),
+        controlnet_processor_res: int = Body(
+            512, title="Controlnet Processor Resolution"
+        ),
+        controlnet_threshold_a: float = Body(64, title="Controlnet Threshold a"),
+        controlnet_threshold_b: float = Body(64, title="Controlnet Threshold b"),
     ):
-        controlnet_module = global_state.reverse_preprocessor_aliases.get(controlnet_module, controlnet_module)
+        controlnet_module = global_state.reverse_preprocessor_aliases.get(
+            controlnet_module, controlnet_module
+        )
 
         if controlnet_module not in cached_cn_preprocessors:
-            raise HTTPException(
-                status_code=422, detail="Module not available")
+            raise HTTPException(status_code=422, detail="Module not available")
 
         if len(controlnet_input_images) == 0:
-            raise HTTPException(
-                status_code=422, detail="No image selected")
+            raise HTTPException(status_code=422, detail="No image selected")
 
-        logger.info(f"Detecting {str(len(controlnet_input_images))} images with the {controlnet_module} module.")
+        logger.info(
+            f"Detecting {str(len(controlnet_input_images))} images with the {controlnet_module} module."
+        )
 
         results = []
 
@@ -103,11 +113,19 @@ def controlnet_api(_: gr.Blocks, app: FastAPI):
 
         for input_image in controlnet_input_images:
             img = external_code.to_base64_nparray(input_image)
-            results.append(processor_module(img, res=controlnet_processor_res, thr_a=controlnet_threshold_a, thr_b=controlnet_threshold_b)[0])
+            results.append(
+                processor_module(
+                    img,
+                    res=controlnet_processor_res,
+                    thr_a=controlnet_threshold_a,
+                    thr_b=controlnet_threshold_b,
+                )[0]
+            )
 
         global_state.cn_preprocessor_unloadable.get(controlnet_module, lambda: None)()
         results64 = list(map(encode_to_base64, results))
         return {"images": results64, "info": "Success"}
+
 
 try:
     import modules.script_callbacks as script_callbacks
