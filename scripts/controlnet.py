c583b3a1b2d69ca436729cec6d7f9b879d0fa4fa
lvmin <lyuminzhang@outlook.com>
2023-09-06 12:14:46 -0700

    fix tiled vae

--- scripts/controlnet.py
+++ scripts/controlnet.py
@@ -961,19 +961,26 @@ class Script(scripts.Script, metaclass=(
         self.detected_map = detected_maps
         self.post_processors = post_processors
 
+    def controlnet_hack(self, p):
+        t = time.time()
+        self.controlnet_main_entry(p)
+        if len(self.enabled_units) > 0:
+            logger.info(f'ControlNet Hooked - Time = {time.time() - t}')
+        return
+
+    def process(self, p, *args, **kwargs):
+        if p.refiner_checkpoint is None:
+            self.controlnet_hack(p)
+        return
+
     def before_process_batch(self, p, *args, **kwargs):
         if self.noise_modifier is not None:
             p.rng = HackedImageRNG(rng=p.rng,
                                    noise_modifier=self.noise_modifier,
                                    sd_model=p.sd_model)
         self.noise_modifier = None
-
-        t = time.time()
-        self.controlnet_main_entry(p)
-
-        if len(self.enabled_units) > 0:
-            logger.info(f'ControlNet Hooked - Time = {time.time() - t}')
-
+        if p.refiner_checkpoint is not None:
+            self.controlnet_hack(p)
         return
 
     def postprocess_batch(self, p, *args, **kwargs):
