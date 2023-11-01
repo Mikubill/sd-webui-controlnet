620ca71da36e5769f40e3410eb4d279ea42d421a
青龍聖者@bdsqlsz <qinglongshengzhe@gmail.com>
2023-11-01 23:28:41 +0800

    fix int

--- annotator/zoe/zoedepth/models/base_models/midas_repo/midas/backbones/beit.py
+++ annotator/zoe/zoedepth/models/base_models/midas_repo/midas/backbones/beit.py
@@ -44,7 +44,7 @@ def _get_rel_pos_bias(self, window_size):
     old_sub_table = old_relative_position_bias_table[:old_num_relative_distance - 3]
 
     old_sub_table = old_sub_table.reshape(1, old_width, old_height, -1).permute(0, 3, 1, 2)
-    new_sub_table = F.interpolate(old_sub_table, size=(new_height, new_width), mode="bilinear")
+    new_sub_table = F.interpolate(old_sub_table, size=(int(new_height), int(new_width)), mode="bilinear")
     new_sub_table = new_sub_table.permute(0, 2, 3, 1).reshape(new_num_relative_distance - 3, -1)
 
     new_relative_position_bias_table = torch.cat(
