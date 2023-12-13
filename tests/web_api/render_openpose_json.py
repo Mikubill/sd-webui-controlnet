a13bd2febe4fae1184b548504957d19a65425a89
Chenlei Hu <chenlei.hu@mail.utoronto.ca>
2023-12-12 22:07:52 -0500

    Add render_openposes_json API endpoint (#2325)
    * Add render_openpose_json API endpoint
    
    * nits

---
+++ tests/web_api/render_openpose_json.py
@@ -0,0 +1,42 @@
+import requests
+import unittest
+import importlib
+import json
+from pathlib import Path
+
+utils = importlib.import_module("extensions.sd-webui-controlnet.tests.utils", "utils")
+
+
+def render(poses):
+    return requests.post(
+        utils.BASE_URL + "/controlnet/render_openpose_json", json=poses
+    ).json()
+
+
+with open(Path(__file__).parent / "pose.json", "r") as f:
+    pose = json.load(f)
+
+
+class TestDetectEndpointWorking(unittest.TestCase):
+    def test_render_single(self):
+        res = render([pose])
+        self.assertEqual(res["info"], "Success")
+        self.assertEqual(len(res["images"]), 1)
+
+    def test_render_multiple(self):
+        res = render([pose, pose])
+        self.assertEqual(res["info"], "Success")
+        self.assertEqual(len(res["images"]), 2)
+
+    def test_render_no_pose(self):
+        res = render([])
+        self.assertNotEqual(res["info"], "Success")
+
+    def test_render_invalid_pose(self):
+        res = render([{"foo": 10, "bar": 100}])
+        self.assertNotIn("info", res)
+        self.assertNotIn("images", res)
+
+
+if __name__ == "__main__":
+    unittest.main()
