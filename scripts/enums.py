11019f48209bdebb91728e2a3a6b438595561ff6
Chenlei Hu <chenlei.hu@mail.utoronto.ca>
2023-12-30 22:35:52 +0000

    :sparkles: Support advanced_weighting in API (#2368)
    * :sparkles: Support advanced_weighting in API
    
    * :memo: add note
    
    * :bug: nits
    
    * :memo: Add examples

--- scripts/enums.py
+++ scripts/enums.py
@@ -25,6 +25,20 @@ class StableDiffusionVersion(Enum):
 
         return StableDiffusionVersion.UNKNOWN
 
+    def encoder_block_num(self) -> int:
+        match self:
+            case StableDiffusionVersion.SD1x:
+                return 12
+            case StableDiffusionVersion.SD2x:
+                return 12
+            case StableDiffusionVersion.SDXL:
+                return 9
+            case StableDiffusionVersion.UNKNOWN:
+                return 12
+
+    def controlnet_layer_num(self) -> int:
+        return self.encoder_block_num() + 1
+
 
 class ControlModelType(Enum):
     """
