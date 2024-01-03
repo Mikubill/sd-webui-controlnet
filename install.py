8d68bc43651f8e1563707f210c00110398b6fd57
huchenlei <chenlei.hu@mail.utoronto.ca>
2024-01-03 10:37:37 -0500

    Install script for hand refiner dependenceis

--- install.py
+++ install.py
@@ -1,14 +1,17 @@
 import launch
-import os
 import pkg_resources
+from pathlib import Path
 from typing import Tuple, Optional
 
-req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
+main_req_file = Path(__file__).parent / "requirements.txt"
+hand_refiner_req_file = (
+    Path(__file__).parent / "annotator" / "hand_refiner_portable" / "requirements.txt"
+)
 
 
 def comparable_version(version: str) -> Tuple:
-    return tuple(version.split('.'))
-    
+    return tuple(version.split("."))
+
 
 def get_installed_version(package: str) -> Optional[str]:
     try:
@@ -17,22 +20,40 @@ def get_installed_version(package: str) -> Optional[str]:
         return None
 
 
-with open(req_file) as file:
-    for package in file:
-        try:
-            package = package.strip()
-            if '==' in package:
-                package_name, package_version = package.split('==')
-                installed_version = get_installed_version(package_name)
-                if installed_version != package_version:
-                    launch.run_pip(f"install -U {package}", f"sd-webui-controlnet requirement: changing {package_name} version from {installed_version} to {package_version}")
-            elif '>=' in package:
-                package_name, package_version = package.split('>=')
-                installed_version = get_installed_version(package_name)
-                if not installed_version or comparable_version(installed_version) < comparable_version(package_version):
-                    launch.run_pip(f"install -U {package}", f"sd-webui-controlnet requirement: changing {package_name} version from {installed_version} to {package_version}")
-            elif not launch.is_installed(package):
-                launch.run_pip(f"install {package}", f"sd-webui-controlnet requirement: {package}")
-        except Exception as e:
-            print(e)
-            print(f'Warning: Failed to install {package}, some preprocessors may not work.')
+def install_requirements(req_file):
+    with open(req_file) as file:
+        for package in file:
+            try:
+                package = package.strip()
+                if "==" in package:
+                    package_name, package_version = package.split("==")
+                    installed_version = get_installed_version(package_name)
+                    if installed_version != package_version:
+                        launch.run_pip(
+                            f"install -U {package}",
+                            f"sd-webui-controlnet requirement: changing {package_name} version from {installed_version} to {package_version}",
+                        )
+                elif ">=" in package:
+                    package_name, package_version = package.split(">=")
+                    installed_version = get_installed_version(package_name)
+                    if not installed_version or comparable_version(
+                        installed_version
+                    ) < comparable_version(package_version):
+                        launch.run_pip(
+                            f"install -U {package}",
+                            f"sd-webui-controlnet requirement: changing {package_name} version from {installed_version} to {package_version}",
+                        )
+                elif not launch.is_installed(package):
+                    launch.run_pip(
+                        f"install {package}",
+                        f"sd-webui-controlnet requirement: {package}",
+                    )
+            except Exception as e:
+                print(e)
+                print(
+                    f"Warning: Failed to install {package}, some preprocessors may not work."
+                )
+
+
+install_requirements(main_req_file)
+install_requirements(hand_refiner_req_file)
