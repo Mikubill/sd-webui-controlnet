dad60dee87022754dda5f99db4b8bfd19809885e
huchenlei <chenlei.hu@mail.utoronto.ca>
2024-01-14 17:33:55 -0500

    More tests

--- tests/web_api/full_coverage/inpaint_test.py
+++ tests/web_api/full_coverage/inpaint_test.py
@@ -1,6 +1,12 @@
 import unittest
 import pytest
-from .template import is_full_coverage, APITestTemplate, girl_img, mask_img
+from .template import (
+    is_full_coverage,
+    APITestTemplate,
+    girl_img,
+    mask_img,
+    mask_small_img,
+)
 
 
 class TestInpaintFullCoverage(unittest.TestCase):
@@ -27,7 +33,9 @@ class TestInpaintFullCoverage(unittest.TestCase):
 
             unit["model"] = "control_v11p_sd15_inpaint [ebff9138]"
 
-            for i_resize, resize_mode in enumerate(("Just Resize", "Crop and Resize", "Resize and Fill")):
+            for i_resize, resize_mode in enumerate(
+                ("Just Resize", "Crop and Resize", "Resize and Fill")
+            ):
                 # Gen 512x768(input image size) for resize.
                 if resize_mode == "Crop and Resize":
                     payload["height"] = 768
@@ -64,6 +72,75 @@ class TestInpaintFullCoverage(unittest.TestCase):
                                 unit_overrides=unit,
                             ).exec()
                         )
+
+    def test_inpaint_no_mask(self):
+        """Inpaint should fail if no mask is provided."""
+        for gen_type in ("img2img", "txt2img"):
+            if gen_type == "img2img":
+                payload = {
+                    "init_images": [girl_img],
+                }
+                unit = {}
+            else:
+                payload = {}
+                unit = {
+                    "image": {
+                        "image": girl_img,
+                    }
+                }
+            unit["model"] = "control_v11p_sd15_inpaint [ebff9138]"
+            unit["module"] = "inpaint_only"
+            with self.subTest(gen_type=gen_type):
+                self.assertFalse(
+                    APITestTemplate(
+                        f"{gen_type}_no_mask_fail",
+                        gen_type,
+                        payload_overrides=payload,
+                        unit_overrides=unit,
+                    ).exec()
+                )
+
+    def test_inpaint_double_mask(self):
+        """When mask is provided for both a1111 img2img input and ControlNet
+        unit input, ControlNet input mask should be used."""
+        self.assertTrue(
+            APITestTemplate(
+                f"img2img_double_mask",
+                "img2img",
+                payload_overrides={
+                    "init_images": [girl_img],
+                    "mask": mask_img,
+                },
+                unit_overrides={
+                    "image": {
+                        "image": girl_img,
+                        "mask": mask_small_img,
+                    },
+                    "model": "control_v11p_sd15_inpaint [ebff9138]",
+                    "module": "inpaint",
+                },
+            ).exec()
+        )
+
+    def test_img2img_mask_on_unit(self):
+        """ Usecase for inpaint_global_harmonious. """
+        self.assertTrue(
+            APITestTemplate(
+                f"img2img_mask_on_unit",
+                "img2img",
+                payload_overrides={
+                    "init_images": [girl_img],
+                },
+                unit_overrides={
+                    "image": {
+                        "image": girl_img,
+                        "mask": mask_small_img,
+                    },
+                    "model": "control_v11p_sd15_inpaint [ebff9138]",
+                    "module": "inpaint",
+                },
+            ).exec()
+        )
 
 if __name__ == "__main__":
     unittest.main()
