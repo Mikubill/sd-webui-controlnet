add119b61d8e718cc5058aa70ff5ec4f7ef5edb9
h3rmit <prominio@yahoo.com>
2024-02-17 06:36:00 +0200

    fix: backward compatibility of clipvision with webui v1.3.0

--- annotator/clipvision/__init__.py
+++ annotator/clipvision/__init__.py
@@ -3,10 +3,13 @@ import cv2
 import torch
 
 from modules import devices
-from modules.modelloader import load_file_from_url
 from annotator.annotator_path import models_path
 from transformers import CLIPVisionModelWithProjection, CLIPVisionConfig, CLIPImageProcessor
 
+try:
+    from modules.modelloader import load_file_from_url
+except ImportError:
+    from basicsr.utils.download_util import load_file_from_url
 
 config_clip_g = {
   "attention_dropout": 0.0,
