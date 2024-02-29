976c595150bc3b98496be5039b67b31382f5964a
Chenlei Hu <chenlei.hu@mail.utoronto.ca>
2024-02-29 03:44:10 +0000

    Add ruff linter dep (#2666)

--- internal_controlnet/external_code.py
+++ internal_controlnet/external_code.py
@@ -134,7 +134,7 @@ def pixel_perfect_resolution(
     else:
         estimation = max(k0, k1) * float(min(raw_H, raw_W))
 
-    logger.debug(f"Pixel Perfect Computation:")
+    logger.debug("Pixel Perfect Computation:")
     logger.debug(f"resize_mode = {resize_mode}")
     logger.debug(f"raw_H = {raw_H}")
     logger.debug(f"raw_W = {raw_W}")
