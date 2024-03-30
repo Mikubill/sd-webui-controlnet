5d4671ea5ba6aff2fbec72b80bc97747c5f0660a
huchenlei <chenlei.hu@mail.utoronto.ca>
2024-03-30 13:48:50 -0400

    Migrate detection tests

--- tests/web_api/detect_test.py
+++ tests/web_api/detect_test.py
@@ -1,47 +1,74 @@
+import pytest
 import requests
-import unittest
-import importlib
-utils = importlib.import_module(
-    'extensions.sd-webui-controlnet.tests.utils', 'utils')
+from typing import List
+
+from .template import (
+    APITestTemplate,
+    realistic_girl_face_img,
+    save_base64,
+    get_dest_dir,
+    disable_in_cq,
+)
 
 
-class TestDetectEndpointWorking(unittest.TestCase):
-    def setUp(self):
-        self.base_detect_args = {
-            "controlnet_module": "canny",
-            "controlnet_input_images": [utils.readImage("test/test_files/img2img_basic.png")],
-            "controlnet_processor_res": 512,
-            "controlnet_threshold_a": 0,
-            "controlnet_threshold_b": 0,
-        }
-
-    def test_detect_with_invalid_module_performed(self):
-        detect_args = self.base_detect_args.copy()
-        detect_args.update({
-            "controlnet_module": "INVALID",
-        })
-        self.assertEqual(utils.detect(detect_args).status_code, 422)
-
-    def test_detect_with_no_input_images_performed(self):
-        detect_args = self.base_detect_args.copy()
-        detect_args.update({
-            "controlnet_input_images": [],
-        })
-        self.assertEqual(utils.detect(detect_args).status_code, 422)
-
-    def test_detect_with_valid_args_performed(self):
-        detect_args = self.base_detect_args
-        response = utils.detect(detect_args)
-
-        self.assertEqual(response.status_code, 200)
-        
-    def test_detect_invert(self):
-        detect_args = self.base_detect_args.copy()
-        detect_args["controlnet_module"] = "invert"
-        response = utils.detect(detect_args)
-        self.assertEqual(response.status_code, 200)
-        self.assertNotEqual(response.json()['images'], [""])
+def get_modules() -> List[str]:
+    return requests.get(APITestTemplate.BASE_URL + "controlnet/module_list").json()[
+        "module_list"
+    ]
 
 
-if __name__ == "__main__":
-    unittest.main()
+def detect_template(payload, output_name: str, status: int = 200):
+    url = APITestTemplate.BASE_URL + "controlnet/detect"
+    resp = requests.post(url, json=payload)
+    assert resp.status_code == status
+    if status != 200:
+        return
+
+    resp_json = resp.json()
+    assert "images" in resp_json
+    assert len(resp_json["images"]) == len(payload["controlnet_input_images"])
+    if not APITestTemplate.is_cq_run:
+        for i, img in enumerate(resp_json["images"]):
+            if img == "Detect result is not image":
+                continue
+            dest = get_dest_dir() / f"{output_name}_{i}.png"
+            save_base64(img, dest)
+    return resp_json
+
+
+@disable_in_cq
+@pytest.mark.parametrize("module", get_modules())
+def test_detect_all_modules(module: str):
+    payload = dict(
+        controlnet_input_images=[realistic_girl_face_img],
+        controlnet_module=module,
+    )
+    detect_template(payload, f"detect_{module}")
+
+
+def test_detect_simple():
+    detect_template(
+        dict(
+            controlnet_input_images=[realistic_girl_face_img],
+            controlnet_module="canny",  # Canny does not require model download.
+        ),
+        "simple_detect",
+    )
+
+
+def test_detect_multiple_inputs():
+    detect_template(
+        dict(
+            controlnet_input_images=[realistic_girl_face_img, realistic_girl_face_img],
+            controlnet_module="canny",  # Canny does not require model download.
+        ),
+        "multiple_inputs_detect",
+    )
+
+
+def test_detect_with_invalid_module():
+    detect_template({"controlnet_module": "INVALID"}, "invalid module", 422)
+
+
+def test_detect_with_no_input_images():
+    detect_template({"controlnet_input_images": []}, "invalid module", 422)
