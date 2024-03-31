500457659cfef51d125f2d4f7e46b1ebd7f746a2
Chenlei Hu <chenlei.hu@mail.utoronto.ca>
2024-03-31 16:14:00 +0000

    :memo: Update to version v1.1.443 (#2722)

--- scripts/controlnet_version.py
+++ scripts/controlnet_version.py
@@ -1,6 +1,6 @@
 from scripts.logging import logger
 
-version_flag = 'v1.1.442'
+version_flag = 'v1.1.443'
 
 logger.info(f"ControlNet {version_flag}")
 # A smart trick to know if user has updated as well as if user has restarted terminal.
