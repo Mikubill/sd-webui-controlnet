671cec6aa25774499fafce2445ba29151a6923c4
mimichan0220 <mimichan0220@gmail.com>
2025-05-05 22:28:47 +0900

    Update controlnet_version.py
    super fine concept art, extremely detailed, a heavy mech, hard surface, hawken, sci fi battlefield

--- scripts/controlnet_version.py
+++ scripts/controlnet_version.py
@@ -1,4 +1,4 @@
-version_flag = 'v1.1.455'
+version_flag = 'v1.1.456'
 # A smart trick to know if user has updated as well as if user has restarted terminal.
 # Note that in "controlnet.py" we do NOT use "importlib.reload" to reload this "controlnet_version.py"
 # This means if user did not completely restart terminal, the "version_flag" will be the previous version.
